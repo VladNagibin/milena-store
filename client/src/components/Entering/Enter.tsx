3bec8830bbd3390f0b22160db54ab3a1c56d0dcc
Vladislav Nagibin <vnagibin@leantech.ai>
2022-12-19 12:45:04 +0600

    https

--- client/src/components/Entering/Enter.tsx
+++ client/src/components/Entering/Enter.tsx
@@ -1,52 +1,62 @@
-import React, { useState } from 'react'
-import { useHttp } from '../../hooks/http.hook'
-import { toast } from 'react-toastify'
-import { useAppDispatch } from '../../hooks/redux.hook'
-import { enter, ILoginData } from '../../reducers/authReducer'
+import React, { useState } from 'react';
+import { useHttp } from '../../hooks/http.hook';
+import { toast } from 'react-toastify';
+import { useAppDispatch } from '../../hooks/redux.hook';
+import { enter, ILoginData } from '../../reducers/authReducer';
 
 export default function Enter() {
-    const [form, setForm] = useState({
-        login: '',
-        password: ''
-    })
-    const { loading, request } = useHttp()
-    const formHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setForm({ ...form, [event.target.name]: event.target.value })
+  const [form, setForm] = useState({
+    login: '',
+    password: '',
+  });
+  const { loading, request } = useHttp();
+  const formHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setForm({ ...form, [event.target.name]: event.target.value });
+  };
+  const dispatch = useAppDispatch();
+
+  const login = () => {
+    if (form.login == '') {
+      toast.error('Вы не ввели логин');
     }
-    const dispatch = useAppDispatch()
-
-    const login = () => {
-        if (form.login == '') {
-            toast.error('Вы не ввели логин')
-        }
-        if (form.password == '') {
-            toast.error('Вы не ввели пароль')
-        }
-
-        try {
-            request<{
-                success: boolean
-                data: ILoginData
-                error?: string
-            }>('/users', 'POST', form).then(json => {
-                if (json.success) {
-                    dispatch(enter(json.data))
-                    toast.success('Вы вошли в аккаунт')
-                } else {
-                    toast.error(json.error)
-                }
-            })
-        } catch (e) {
-            toast.error('При попытке регистрации произошла ошибка')
-        }
-
+    if (form.password == '') {
+      toast.error('Вы не ввели пароль');
     }
-    return (
-        <div className='enter'>
-            <input name='login' type={'text'} placeholder='Логин/Email' value={form.login} onChange={formHandler} />
-            <input name='password' type='password' placeholder='Пароль' value={form.password} onChange={formHandler} />
-            <button onClick={login}>Войти</button>
 
-        </div>
-    )
+    try {
+      request<{
+        success: boolean;
+        data: ILoginData;
+        error?: string;
+      }>('/users', 'POST', form).then((json) => {
+        if (json.success) {
+          dispatch(enter(json.data));
+          toast.success('Вы вошли в аккаунт');
+        } else {
+          toast.error(json.error);
+        }
+      });
+    } catch (e) {
+      toast.error('При попытке регистрации произошла ошибка');
+    }
+  };
+  return (
+    <div className="enter">
+      <input
+        name="login"
+        type={'text'}
+        placeholder="Логин/Email"
+        value={form.login}
+        onChange={formHandler}
+      />
+      <input
+        name="password"
+        type="password"
+        placeholder="Пароль"
+        value={form.password}
+        onChange={formHandler}
+      />
+      <button onClick={login}>Войти</button>
+    </div>
+  );
 }
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { static as ExStatic } from 'express';
-import * as path from 'path'
-const isDev = process.env.NODE_ENV == 'development'
+import fs from 'fs';
+import * as path from 'path';
+const isDev = process.env.NODE_ENV == 'development';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  let privateKey = fs
+    .readFileSync('../../etc/letsencrypt/live/choose-votes.ru/privkey.pem')
+    .toString();
+  let certificate = fs
+    .readFileSync('../../etc/letsencrypt/live/choose-votes.ru/cert.pem')
+    .toString();
+
+  let httpsOptions = { key: privateKey, cert: certificate };
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   app.enableCors();
-  if(!isDev){
-    app.use('/',ExStatic(path.join(__dirname,'public')))
-    console.log('production mode')
+  if (!isDev) {
+    app.use('/', ExStatic(path.join(__dirname, 'public')));
+    console.log('production mode');
   }
-  await app.listen(isDev?process.env.DEV_PORT:process.env.PROD_PORT);
-  
-  console.log(`server is running on:${isDev?process.env.DEV_PORT:process.env.PROD_PORT}`)
+  await app.listen(isDev ? process.env.DEV_PORT : process.env.PROD_PORT);
+
+  console.log(
+    `server is running on:${
+      isDev ? process.env.DEV_PORT : process.env.PROD_PORT
+    }`,
+  );
 }
 bootstrap();
